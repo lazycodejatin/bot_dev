f8e834c5c7556f4b9c68da6fa5856745be06b33e
lazycodejatin <lazycodejatin@gmail.com>
2025-04-16 16:16:51 +0530

    Update motor_Driverspin.cpp

--- motor_Driverspin.cpp
+++ motor_Driverspin.cpp
@@ -1,65 +1,145 @@
-// Motor driver pins
+// Motor control pins
 const int IN1 = 2;
 const int IN2 = 3;
 const int IN3 = 4;
 const int IN4 = 5;
 
-// Bluetooth command from phone
 char command;
+bool forwardPressed = false;
+bool leftPressed = false;
+bool rightPressed = false;
+bool backwardPressed = false;
 
 void setup() {
-  // Set motor pins as output
+  // Initialize motor pins
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
 
-  // Start serial communication with Bluetooth module
-  Serial.begin(9600);  // HC-05 default baud rate
+  // Start Serial communication for bluetooth
+  Serial.begin(9600);//default
 }
 
 void loop() {
   if (Serial.available()) {
     command = Serial.read();
 
-    // Forward
+    
     if (command == 'F') {
-      digitalWrite(IN1, HIGH);
-      digitalWrite(IN2, LOW);
-      digitalWrite(IN3, HIGH);
-      digitalWrite(IN4, LOW);
+      forwardPressed = true;
     }
-
-    // Backward
     else if (command == 'B') {
-      digitalWrite(IN1, LOW);
-      digitalWrite(IN2, HIGH);
-      digitalWrite(IN3, LOW);
-      digitalWrite(IN4, HIGH);
+      backwardPressed = true;
     }
-
-    // Left
     else if (command == 'L') {
-      digitalWrite(IN1, LOW);
-      digitalWrite(IN2, HIGH);
-      digitalWrite(IN3, HIGH);
-      digitalWrite(IN4, LOW);
+      leftPressed = true;
     }
-
-    // Right
     else if (command == 'R') {
-      digitalWrite(IN1, HIGH);
-      digitalWrite(IN2, LOW);
-      digitalWrite(IN3, LOW);
-      digitalWrite(IN4, HIGH);
+      rightPressed = true;
     }
-
-    // Stop
-    else if (command == 'S') {
-      digitalWrite(IN1, LOW);
-      digitalWrite(IN2, LOW);
-      digitalWrite(IN3, LOW);
-      digitalWrite(IN4, LOW);
+    else if (command == 'S') { // Stop
+      stopMotors();
     }
   }
+
+ 
+  if (forwardPressed && leftPressed) {
+    forwardLeft();
+  }
+  else if (forwardPressed && rightPressed) {
+    forwardRight();
+  }
+  else if (backwardPressed && leftPressed) {
+    backwardLeft();
+  }
+  else if (backwardPressed && rightPressed) {
+    backwardRight();
+  }
+  else if (forwardPressed) {
+    forward();
+  }
+  else if (backwardPressed) {
+    backward();
+  }
+  else if (leftPressed) {
+    left();
+  }
+  else if (rightPressed) {
+    right();
+  }
+
+ 
+  forwardPressed = false;
+  backwardPressed = false;
+  leftPressed = false;
+  rightPressed = false;
+}
+
+
+void forward() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void backward() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+void left() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void right() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+void stopMotors() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
+
+void forwardLeft() {
+  // Forward-left: Left motor forward, Right motor backward
+  digitalWrite(IN1, HIGH); 
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);  
+  digitalWrite(IN4, HIGH);
+}
+
+void forwardRight() {
+  // Forward-right: Left motor forward, Right motor forward
+  digitalWrite(IN1, HIGH); 
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void backwardLeft() {
+  // Backward-left: Left motor backward, Right motor forward
+  digitalWrite(IN1, LOW); 
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void backwardRight() {
+  // Backward-right: Left motor backward, Right motor backward
+  digitalWrite(IN1, LOW);  
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
 }
